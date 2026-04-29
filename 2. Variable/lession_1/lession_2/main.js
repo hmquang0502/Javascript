d960eb8949bcd10059ffc57de042fce173ef51c6
hmquang0502 <quang05022003@gmail.com>
2026-04-29 22:22:47 +0700

    ham build-in

---
+++ 2. Variable/lession_1/lession_2/main.js
@@ -0,0 +1,14 @@
+console.log('Day la in ra'); // ham in ra
+
+confirm('Xac nhan ban du tuoi!'); // ham confirm
+
+prompt('Xac nhan ban du tuoi'); // ham prompt
+
+setTimeout(function () { // ham time out chay sau 1 khoang thoi gian
+    alert('Thong bao')
+}, 1000);
+
+
+setInterval(function () { // ham Interval chay dinh ki
+    console.log('Day la thong bao')
+}, 1000);
