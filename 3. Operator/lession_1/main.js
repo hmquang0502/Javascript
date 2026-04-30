dab111b000e8921baced2ab8d208c020d9d72217
hmquang0502 <quang05022003@gmail.com>
2026-04-30 22:17:37 +0700

    operator

---
+++ 3. Operator/lession_1/main.js
@@ -0,0 +1,13 @@
+var a = 3;
+var b = 2;
+
+if (a > b) {
+    alert('a lon hon b');
+};
+
+/**
+ *  Prefix & Postfix
+ * 
+ *  ++ o truoc thi tang len roi moi thuc hien
+ *  ++ o sau thi thuc hien roi moi tang len
+ */
